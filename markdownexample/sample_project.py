b9776af1f7e6e3cdac1bbe90988c2238ed7cc1c5
saikrishnna29 <panugantisaikrishna@icloud.com>
2023-09-04 20:54:02 +0530

    new changes

---
+++ markdownexample/sample_project.py
@@ -0,0 +1,10 @@
+r'''
+# What is Sagemaker?
+
+## Overview of machine learning with Amazon SageMaker
+
+In machine learning, you "teach" a computer to make predictions, or inferences. 
+First, you use an algorithm and example data to train a model. 
+Then you integrate your model into your application to generate inferences in real time and at scale. 
+In a production environment, a model typically learns from millions of example data items and produces inferences in hundreds to less than 20 milliseconds.
+'''
